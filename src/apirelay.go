1da62665bbdd0dd81be3bb85bc9ad7ca21a4217e
Schneider <0861453002@HQ.DHS.GOV>
2022-09-26 12:12:48 -0400

    fix typo in new String

--- src/apirelay.go
+++ src/apirelay.go
@@ -32,7 +32,7 @@ func InitMSGraph() {
 	files := strings.Split(*apiCredentialFiles, ",")
 	for _, file := range files {
 		fileContents, err := os.ReadFile(file)
-    fc := newString(fileContents)
+    fc := new String(fileContents)
     log.WithFields(logrus.Fields{
       "fileContents": fc,
     }).Info("File contents")
