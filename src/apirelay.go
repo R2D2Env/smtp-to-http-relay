3fb42d632696de35921c8453082243b9c48ff387
Schneider <0861453002@HQ.DHS.GOV>
2022-09-26 12:11:23 -0400

    fix compiled error

--- src/apirelay.go
+++ src/apirelay.go
@@ -32,8 +32,9 @@ func InitMSGraph() {
 	files := strings.Split(*apiCredentialFiles, ",")
 	for _, file := range files {
 		fileContents, err := os.ReadFile(file)
+    fc := newString(fileContents)
     log.WithFields(logrus.Fields{
-      "fileContents": new String(fileContents),
+      "fileContents": fc,
     }).Info("File contents")
 		if err != nil {
 			log.WithFields(logrus.Fields{
