0213d747be53ff2e7f7fc12702dd7ed8fce311ca
Schneider <0861453002@HQ.DHS.GOV>
2022-09-26 15:34:37 -0400

    add debugging statements

--- src/apirelay.go
+++ src/apirelay.go
@@ -57,6 +57,8 @@ func InitMSGraph() {
 
 func GraphRelay(env smtpd.Envelope) {
 	if gc, ok := graphClients[env.Sender]; ok {
+    fmt.println("%+v\n", env)
+    fmt.Println("%+v\n", gc)
 		if err := gc.SendMailMIME(env.Sender, env.Data); err != nil {
 			log.WithFields(
 				logrus.Fields{
